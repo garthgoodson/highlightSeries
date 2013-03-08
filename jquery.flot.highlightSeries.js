a5f17f76dfc47d123e51b22232572dfc8ce9e4f3
garthgoodson <garth@natero.com>
2013-03-07 16:33:37 -0800

    -Refactoring (mostly indentation). Removed debug/optimize modes.

--- jquery.flot.highlightSeries.js
+++ jquery.flot.highlightSeries.js
@@ -1,161 +1,171 @@
 /*
-Flot plugin for highlighting series.
+  Flot plugin for highlighting series.
 
-	highlightSeries: {
-		autoHighlight: true (default) or false
-		, color: color
-	}
+  highlightSeries: {
+  autoHighlight: true (default) or false
+  , color: color
+  }
 
-If "autoHighlight" is true (the default) and the plot's "hoverable" setting is true
-series are highlighted when the mouse hovers near an item.
-"color" is the color of the highlighted series (default is "red").
+  If "autoHighlight" is true (the default) and the plot's "hoverable" setting is true
+  series are highlighted when the mouse hovers near an item.
+  "color" is the color of the highlighted series (default is "red").
 
-The plugin also adds two public methods that allow you to highlight and
-unhighlight a series manually by specifying a series by label, index or object.
+  The plugin also adds two public methods that allow you to highlight and
+  unhighlight a series manually by specifying a series by label, index or object.
 
-	- highlightSeries(series, [color])
+  - highlightSeries(series, [color])
 
-	- unHighlightSeries(series)
+  - unHighlightSeries(series)
 */
-
+var count = 1;
 (function ($) {
-	var log = (function () {
-		var out = $("#out");
-		return function () {
-			if (!arguments) { return; }
-			var msg = Array.prototype.slice.call(arguments).join(" ");
-			if (!out.length) {
-				out = $("#out");
-			}
-			if (out.length) {
-				out.text(msg);
-			}
-		};
-	})();
-
-	var options = {
-		highlightSeries: {
-			autoHighlight: true
-			, color: "black"
-			, _optimized: true
-			, _debug: false
-		}
+    var log = (function () {
+	var out = $("#out");
+	return function () {
+	    if (!arguments) { return; }
+	    var msg = Array.prototype.slice.call(arguments).join(" ");
+	    if (!out.length) {
+		out = $("#out");
+	    }
+	    if (out.length) {
+		out.text(msg);
+	    }
 	};
+    })();
 
-	function init(plot) {
-		var highlightedSeries = {};
-		var originalColors = {};
+    var options = {
+	highlightSeries: {
+	    autoHighlight: true
+	    , color: "blue"
+            , highlightCallback: null
+            , unHighlightCallback: null
+            , scale: 0.75
+	}
+    };
 
-		function highlightSeries(series, color) {
-			var
-				seriesAndIndex = getSeriesAndIndex(series)
-				, options = plot.getOptions().highlightSeries;
+    function init(plot) {
+	var highlightedSeries = {};
+	var originalColors = {};
+        var originalSeriesSettings = {};
+        var hiddenSeries = {};
+	var lastHighlighted = null;
 
-			series = seriesAndIndex[1];
+	function highlightSeries(series) {
+	    var seriesAndIndex = getSeriesAndIndex(series)
+	    var options = plot.getOptions().highlightSeries;
 
-			highlightedSeries[seriesAndIndex[0]] = series;
-			originalColors[seriesAndIndex[0]] = series.color;
+	    series = seriesAndIndex[1];
 
-			series.color = color || options.color;
+	    highlightedSeries[seriesAndIndex[0]] = series;
+	    originalColors[seriesAndIndex[0]] = series.color;
 
-			if (options._debug) { var start = new Date(); }
-			if (options._optimized) {
-				if (plot.drawOverlay && options._debug) {
-					plot.drawOverlay();
-				}
-				else {
-					plot.triggerRedrawOverlay();
-				}
-			}
-			else {
-				plot.draw();
-			}
-			if (options._debug) { 
-				log("Time taken to highlight:", (new Date()).getTime() - start.getTime(), "ms");
-			}
-		};
-		plot.highlightSeries = highlightSeries;
+	    series.color =
+                $.color.parse(series.color).scale('rgb',options.scale).toString();
 
-		function unHighlightSeries(series) {
-			var
-				seriesAndIndex = getSeriesAndIndex(series)
-				, options = plot.getOptions().highlightSeries;
+            lastHighlighted = series;
 
-			seriesAndIndex[1].color = originalColors[seriesAndIndex[0]];
+	    plot.triggerRedrawOverlay();
+	};
+	plot.highlightSeries = highlightSeries;
 
-			if (options._debug) { var start = new Date(); }
-			if (options._optimized) {
-				delete highlightedSeries[seriesAndIndex[0]];
-				if (plot.drawOverlay && options._debug) {
-					plot.drawOverlay();
-				}
-				else {
-					plot.triggerRedrawOverlay();
-				}
-			}
-			else {
-				plot.draw();
-			}
-			if (options._debug) { 
-				log("Time taken to un-highlight:", (new Date()).getTime() - start.getTime(), "ms");
-			}
-		};
-		plot.unHighlightSeries = unHighlightSeries;
+	function unHighlightSeries(series) {
+            if (lastHighlighted == null) {
+                return;
+            }
 
-		plot.hooks.bindEvents.push(function (plot, eventHolder) {
-			if (!plot.getOptions().highlightSeries.autoHighlight) {
-				return;
-			}
-			
-			var lastHighlighted = null;
-			plot.getPlaceholder().bind("plothover", function (evt, pos, item) {
-				if (item && lastHighlighted !== item.series) {
-					for(var seriesIndex in highlightedSeries) {
-						delete highlightedSeries[seriesIndex];
-					}
-					if (lastHighlighted) {
-						unHighlightSeries(lastHighlighted);
-					}
-					lastHighlighted = item.series;
-					highlightSeries(item.series);
-				}
-				else if (!item && lastHighlighted) {
-					unHighlightSeries(lastHighlighted);
-					lastHighlighted = null;
-				}
-			});
-		});
+	    var seriesAndIndex = getSeriesAndIndex(series)
+            if (typeof originalColors[seriesAndIndex[0]] === 'undefined') {
+                return;
+            }
 
-		function getSeriesAndIndex(series) {
-			var allPlotSeries = plot.getData();
-			if (typeof series == "number") {
-				return [series, allPlotSeries[series]];
-			}
-			else {
-				for (var ii = 0; ii < allPlotSeries.length; ii++) {
-					var plotSeries = allPlotSeries[ii];
-					if (
-						plotSeries === series
-						|| plotSeries.label === series
-						|| plotSeries.label === series.label
-					) {
-						return [ii, plotSeries];
-					}
-				}
-			}
+	    seriesAndIndex[1].color = originalColors[seriesAndIndex[0]];
+
+	    delete highlightedSeries[seriesAndIndex[0]];
+	    plot.triggerRedrawOverlay();
+	};
+	plot.unHighlightSeries = unHighlightSeries;
+
+	plot.hooks.bindEvents.push(function (plot, eventHolder) {
+	    if (!plot.getOptions().highlightSeries.autoHighlight) {
+		return;
+	    }
+
+	    plot.getPlaceholder().bind("plothover", plotHover);
+	});
+
+        function plotHover(evt, pos, item) {
+
+	    if (item && lastHighlighted !== item.series) {
+
+		for (var seriesIndex in highlightedSeries) {
+		    delete highlightedSeries[seriesIndex];
 		}
 
-		plot.hooks.drawOverlay.push(function (plot, ctx) {
-			for(var seriesIndex in highlightedSeries) {
-				plot.drawSeries(highlightedSeries[seriesIndex], ctx);
-			}
-		});
+		if (lastHighlighted) {
+		    unHighlightSeries(lastHighlighted);
+                    var cb = plot.getOptions().highlightSeries.unHighlightCallback;
+                    if (typeof (cb) === 'function') {
+			cb (lastHighlighted.label);
+                    }
+		}
+
+		lastHighlighted = item.series;
+		highlightSeries(item.series);
+                var cb = plot.getOptions().highlightSeries.highlightCallback;
+                if (typeof (cb) === 'function') {
+                    cb (item.series.label);
+                }
+	    }
+	    else if (!item && lastHighlighted) {
+		unHighlightSeries(lastHighlighted);
+                var cb = plot.getOptions().highlightSeries.unHighlightCallback;
+                if (typeof (cb) === 'function') {
+                    cb (lastHighlighted.label);
+                }
+		lastHighlighted = null;
+	    }
 	}
 
-	$.plot.plugins.push({
-		init: init,
-		options: options,
-		name: "highlightSeries",
-		version: "1.0"
+	function getSeriesAndIndex(series) {
+	    var allPlotSeries = plot.getData();
+
+	    if (typeof series == "number") {
+		return [series, allPlotSeries[series]];
+	    }
+	    else {
+		for (var ii = 0; ii < allPlotSeries.length; ii++) {
+		    var plotSeries = allPlotSeries[ii];
+
+		    if (
+			plotSeries === series
+			    || plotSeries.label === series
+			    || plotSeries.label === series.label
+		    ) {
+			return [ii, plotSeries];
+		    }
+		}
+	    }
+	}
+
+	plot.hooks.drawOverlay.push(function (plot, ctx) {
+            if (lastHighlighted == null) {
+                return;
+            }
+	    for(var seriesIndex in highlightedSeries) {
+		plot.drawSeries(highlightedSeries[seriesIndex], ctx);
+	    }
 	});
+
+        plot.hooks.shutdown.push(function (plot, eventHolder) {
+	    plot.getPlaceholder().unbind("plothover", plotHover);
+        });
+
+    }
+
+    $.plot.plugins.push({
+	init: init,
+	options: options,
+	name: "highlightSeries",
+	version: "1.0"
+    });
 })(jQuery);
